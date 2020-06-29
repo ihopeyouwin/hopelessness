d8ca05996d9ea1a5073fef83a8662ea4acfd07f6
ikalinaev <i.kalinaev@vironit.co.uk>
2020-06-29 16:58:47 +0300

    styles and useritem& list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
